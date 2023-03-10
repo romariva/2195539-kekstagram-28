312c16fe18c2faea4582be72b6bf5ba04c8651b1
Roman Golubev <golubevrn@gmail.com>
2023-03-10 17:31:07 +0300

    Первая правка

--- js/picture.js
+++ js/picture.js
@@ -2,15 +2,16 @@ import {userEvent} from './data.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const pictureContainer = userEvent();
+const data = userEvent();
+const pictureContainer = document.querySelector('.pictures');
 
 const pictureFragment = document.createDocumentFragment();
 
-pictureContainer.forEach(({url, likes, comments}) => {
+data.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').textContent = url;
-  pictureElement.querySelector('.picture__comments').textContent = likes;
-  pictureElement.querySelector('.picture__likes').textContent = comments;
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureFragment.append(pictureElement);
 });
 
