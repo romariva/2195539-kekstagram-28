0884e4877412d8005871fff68a6028a703cf32c7
Roman Golubev <golubevrn@gmail.com>
2023-03-12 16:45:47 +0300

    Исправление, оборачивает функцией весь код в picture.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 import {userEvent} from './data.js';
 import {renderPictureList} from './picture.js';
+
+const data = userEvent();
+renderPictureList(data);
--- js/picture.js
+++ js/picture.js
@@ -1,20 +1,21 @@
-import {userEvent} from './data.js';
+const renderPictureList = (data) => {
+//находим шаблон и его содержимое
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const pictureContainer = document.querySelector('.pictures');
+  //находим место для вставки фотографий
+  const pictureContainer = document.querySelector('.pictures');
 
-const pictureFragment = document.createDocumentFragment();
+  //создаем фрагмент для вставки
+  const pictureFragment = document.createDocumentFragment();
 
-const data = userEvent();
+  data.forEach(({url, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureFragment.append(pictureElement);
+  });
+  pictureContainer.append(pictureFragment);
+};
 
-data.forEach(({url, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureFragment.append(pictureElement);
-});
-pictureContainer.append(pictureFragment);
-
-const renderPictureList = (data) => {};
 export {renderPictureList};
