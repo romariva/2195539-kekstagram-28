8d2d3f1a47540b4d7291d3e36dee877bd8547e70
Roman Golubev <golubevrn@gmail.com>
2023-03-12 16:00:44 +0300

    Исправление отображения комментариев

--- js/main.js
+++ js/main.js
@@ -1,5 +1,2 @@
 import {userEvent} from './data.js';
-userEvent();
-
-import {pictureContainer} from './picture.js';
-pictureContainer();
+import {renderPictureList} from './picture.js';
--- js/picture.js
+++ js/picture.js
@@ -1,20 +1,20 @@
 import {userEvent} from './data.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-
-const data = userEvent();
 const pictureContainer = document.querySelector('.pictures');
 
 const pictureFragment = document.createDocumentFragment();
 
+const data = userEvent();
+
 data.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
   pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureFragment.append(pictureElement);
 });
-
 pictureContainer.append(pictureFragment);
 
-export {pictureContainer};
+const renderPictureList = (data) => {};
+export {renderPictureList};
