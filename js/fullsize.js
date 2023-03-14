df051f93527fcc95f284dc6b7b07b015f281bd4a
Roman Golubev <golubevrn@gmail.com>
2023-03-14 17:08:56 +0300

    Добавляет событие на .pictures

--- js/fullsize.js
+++ js/fullsize.js
@@ -3,6 +3,7 @@ import {isEscapeKey} from './util.js';
 const bigPictureSection = document.querySelector('.big-picture');
 const closeButton = document.querySelector('.big-picture__cancel');
 const modalOpen = document.querySelector('body');
+const pictureContainer = document.querySelector('.pictures');
 
 const pictureUrl = document.querySelector('.big-picture__img');
 const pictureLike = document.querySelector('.likes-count');
@@ -12,21 +13,24 @@ const commentCounter = document.querySelector('.social__comment-count');
 const commentLoader = document.querySelector('.comments-loader');
 
 //открыть при клике
-bigPictureSection.addEventListener('click', () => {
+pictureContainer.addEventListener('click', () => {
   bigPictureSection.classList.remove('hidden');
   commentCounter.classList.add('hidden');
   commentLoader.classList.add('hidden');
   modalOpen.classList.add('.modal-open');
-  //закрыть при ESC
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      bigPictureSection.classList.add('hidden');
-      commentCounter.classList.remove('hidden');
-      commentLoader.classList.remove('hidden');
-      modalOpen.classList.remove('.modal-open');
-    }
-  });
+
+
+});
+
+//закрыть при ESC
+document.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    bigPictureSection.classList.add('hidden');
+    commentCounter.classList.remove('hidden');
+    commentLoader.classList.remove('hidden');
+    modalOpen.classList.remove('.modal-open');
+  }
 });
 
 //закрыть кнопкой Крестик
