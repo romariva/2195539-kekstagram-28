433ec037ded21e2737c346e15462e3696e1dbe66
Roman Golubev <golubevrn@gmail.com>
2023-04-01 18:58:41 +0300

    Кнопки увелич уменьш масшт и сброс поумолчанию

--- js/scale.js
+++ js/scale.js
@@ -1,4 +1,4 @@
-const DEFOULT_SCALE = 100;
+const DEFAULT_SCALE = 100;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const SCALE_STEP = 25;
@@ -25,12 +25,19 @@ const onSmallerButtonClick = () => {
 };
 
 //кнопка для увеличения изображения
-
 const onBiggerButtonClick = () => {
   const currentValue = parseInt(valueScaleElement.value, 10);
-  let newValue = currentValue - SCALE_STEP;
+  let newValue = currentValue + SCALE_STEP;
   if (newValue < MAX_SCALE) {
     newValue = MAX_SCALE;
   }
   scaleImage(newValue);
 };
+
+//сброс масштаба поумолчанию
+const resetScale = () => scaleImage(DEFAULT_SCALE);
+
+scaleButtonSmaller.addEventListener('click', onSmallerButtonClick);
+scaleButtonBigger.addEventListener('click', onBiggerButtonClick);
+
+export {resetScale};
