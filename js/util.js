a2f9a13bd1b9667ee9dc31a66ae3e235d11d52da
Roman Golubev <golubevrn@gmail.com>
2023-04-03 13:02:38 +0300

    окно ошибки Alert

--- js/util.js
+++ js/util.js
@@ -1,3 +1,25 @@
+const ALERT_SHOW_TIME = 7000;
+
+//окно ошибки сверху экрана
+const showAlert = (message) => {
+  const alert = document.createElement('div');
+  alert.style.zIndex = '100';
+  alert.style.position = 'absolute';
+  alert.style.left = '0';
+  alert.style.top = '0';
+  alert.style.right = '0';
+  alert.style.padding = '10px 3px';
+  alert.style.fontSize = '30px';
+  alert.style.textAlign = 'center';
+  alert.style.backgroundColor = 'red';
+  alert.textContent = message;
+  document.body.append(alert);
+
+  setTimeout(() => {
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 // Получаем рандомное число в заданном диапазоне
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -25,4 +47,4 @@ const createElement = (tagName, attributes = {}, text) => {
   return element;
 };
 
-export {getRandomArrayElement, getRandomInteger, isEscapeKey, getUniqueId, createElement};
+export {getRandomArrayElement, getRandomInteger, isEscapeKey, getUniqueId, createElement, showAlert};
