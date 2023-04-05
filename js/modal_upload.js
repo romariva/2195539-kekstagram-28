1062c55bcaff196fbcf00ae1ffdca9f9643c488e
Roman Golubev <golubevrn@gmail.com>
2023-04-05 13:53:23 +0300

    Правка 11 часть

--- js/modal_upload.js
+++ js/modal_upload.js
@@ -1,20 +1,15 @@
 const body = document.body;
-const modalOk = document.querySelector('#success').content.querySelector('.success');
+const modalSuccess = document.querySelector('#success').content.querySelector('.success');
 const modalError = document.querySelector('#error').content.querySelector('.error');
 
 //модальное окно успешной отправки
 const getModalSuccess = () => {
-  const element = modalOk.querySelector('div');
-  const clonedElement = element.cloneNode(true);
-  body.appendChild(clonedElement);
+  body.appendChild(modalSuccess);
 };
 
 //модальное окно ошибка отправки
 const getModalError = () => {
-  const element = modalError.querySelector('div');
-  const clonedElement = element.cloneNode(true);
-  body.appendChild(clonedElement);
+  body.appendChild(modalError);
 };
-
 
 export {getModalSuccess, getModalError};
