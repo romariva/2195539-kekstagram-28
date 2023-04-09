fdcb4030fbd27761d67b36c34192e8fd384fcc69
Roman Golubev <golubevrn@gmail.com>
2023-04-09 15:48:17 +0300

    Удаляет комментарии из кода

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ import {getData} from './api.js';
 import {init, getFilter} from './filter.js';
 import {loadLocalFile} from './load-picture.js';
 
-//получение данных с сервера
 getData()
   .then((data) => {
     const debouncedRenderPictureList = debounce(renderPictures);
@@ -20,8 +19,6 @@ getData()
     }
   );
 
-//отправка формы
 formSubmit();
 
-//загрузка своей картинки
 loadLocalFile();
