b41c0af461b095d5e4c4023b7b80ddd02768d375
Roman Golubev <golubevrn@gmail.com>
2023-04-09 13:38:57 +0300

    небольшие изменения

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ import {getData} from './api.js';
 import {init, getFilter} from './filter.js';
 import {loadLocalFile} from './load-picture.js';
 
+//получение данных с сервера
 getData()
   .then((data) => {
     const debouncedRenderPictureList = debounce(renderPictures);
