d595153bfacda8044eb3af1a1f157993cdcbf247
Roman Golubev <golubevrn@gmail.com>
2023-04-10 14:33:29 +0300

    Исправление Б24. Количество вызовов циклов минимизировано. chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value); Сделать EFFECTS обьектом чтобы сразу получать свойства эффекта.

--- js/effect.js
+++ js/effect.js
@@ -1,56 +1,50 @@
-const EFFECTS = [
-  {
-    name: 'none',
+const EFFECTS = {
+  none: {
     style: 'none',
     min:0,
     max:100,
     step:1,
     unit:'',
   },
-  {
-    name: 'chrome',
+  chrome: {
     style: 'grayscale',
     min:0,
     max:1,
     step:0.1,
     unit:'',
   },
-  {
-    name: 'sepia',
+  sepia: {
     style: 'sepia',
     min:0,
     max:1,
     step:0.1,
     unit:'',
   },
-  {
-    name: 'marvin',
+  marvin: {
     style: 'invert',
     min:0,
     max:100,
     step:1,
     unit:'%',
   },
-  {
-    name: 'phobos',
+  phobos: {
     style: 'blur',
     min:0,
     max:3,
     step:0.1,
     unit:'px',
   },
-  {
-    name: 'heat',
+  heat: {
     style: 'brightness',
     min:1,
     max:3,
     step:0.1,
     unit:'',
   },
-];
+};
 
 
-const DEFAULT_EFFECT = EFFECTS[0];
+const DEFAULT_EFFECT = EFFECTS.none;
 
 let chosenEffect = DEFAULT_EFFECT;
 
@@ -100,7 +94,7 @@ const onEffectsChange = (evt) => {
   if (!evt.target.classList.contains('effects__radio')) {
     return;
   }
-  chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+  chosenEffect = EFFECTS[evt.target.value];
   imgPreviewElement.className = `effects__preview--${chosenEffect.name}`;
   updateSlider();
 };
